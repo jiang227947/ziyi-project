bf0b799910cb9066b2b189d4405561f96f806acb
jiangzy103 <www.jzywd666@163.com>
2023-01-11 10:23:30 +0800

    [M] 1. 登录页样式修改

--- src/app/pages/login/auth/auth.component.ts
+++ src/app/pages/login/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from "../../../../environments/environment";
+import {Result} from "../../../shared-module/interface/result";
+import {User} from "../../../shared-module/interface/user";
 
 @Component({
   selector: 'app-auth',
@@ -7,10 +11,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() {
+  // bing每日背景图
+  bingBackGroundImage = '../../../../assets/image/login-background.svg';
+
+  constructor(private $http: HttpClient) {
   }
 
   ngOnInit(): void {
+    // 获取bing每日背景图
+    this.$http.get(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1586183781119&pid=hp&uhd=1&uhdwidth=2880&uhdheight=1620`).subscribe((result) => {
+      this.bingBackGroundImage = `https://cn.bing.com${result.images[0].url}`;
+    });
   }
 
 }
